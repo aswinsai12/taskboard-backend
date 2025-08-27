334f742af57be91f780684e4661c153d535169d9
aswin varma <143880238+aswinsai12@users.noreply.github.com>
2025-08-27 20:00:58 +0530

    Update UserRepository.java

--- src/main/java/com/aswin/taskboard/repository/UserRepository.java
+++ src/main/java/com/aswin/taskboard/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.aswin.taskboard.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+  User findByUsername(String username);
 }
